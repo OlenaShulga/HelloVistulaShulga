59cbb5eea5f393eda61ff2afd5cdabd0c1dcdeb2
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2022-10-07 15:21:34 +0300

    Drugi commit

--- src/pl/vistula/helloworldshulga/HelloWorldShulga.java
+++ src/pl/vistula/helloworldshulga/HelloWorldShulga.java
@@ -5,6 +5,7 @@ public class HelloWorldShulga
     public static void main(String[] args)
     {
         System.out.println("Hello Vistula Shulga");
+        System.out.println("Nowy tekst");
 
     }
 }
